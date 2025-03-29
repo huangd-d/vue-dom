8687e3dbaf403f98f9a02e070800ba5568a42c4f
huangxu <609004102@qq.com>
2025-03-29 19:25:00 +0800

    测试commitlint 是否生效

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,9 @@
 export default {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']],
-        'subject-full-stop': [2, 'never', '.'],
-        'subject-case': [0],
-        'header-max-length': [0, 'always', 72]
-    }
+    // rules: {
+    //     'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']],
+    //     'subject-full-stop': [2, 'never', '.'],
+    //     'subject-case': [0],
+    //     'header-max-length': [0, 'always', 72]
+    // }
 };
